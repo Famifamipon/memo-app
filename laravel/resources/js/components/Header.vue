e68b4ba40ee6172aad45c6c4cffa56c8ffe3d041
Famifamipon <uraraka0124@gmail.com>
2026-04-18 18:00:53 +0900

    ヘッダーの作成

---
+++ laravel/resources/js/components/Header.vue
@@ -0,0 +1,16 @@
+<script setup>
+import DocumentSvg from './svgs/DocumentSvg.vue'
+</script>
+
+<template>
+    <header class="bg-white">
+        <div class="flex gap-2 justify-center items-center pt-6 pb-2">
+            <DocumentSvg class="w-10 h-10"/>
+            <p class="text-orange-500 text-3xl inline-block scale-y-110">かんたんメモ</p>
+        </div>
+        <div class="flex justify-center pb-6">
+            <p class="text-xs">思いついたことをサッと記録しよう</p>
+        </div>
+    </header>
+
+</template>
