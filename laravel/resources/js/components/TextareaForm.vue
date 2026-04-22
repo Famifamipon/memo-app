fab04b0716349482c78f484a0504219e44de3855
Famifamipon <uraraka0124@gmail.com>
2026-04-22 16:19:50 +0900

    メモ作成画面のUIを作成した

---
+++ laravel/resources/js/components/TextareaForm.vue
@@ -0,0 +1,30 @@
+<script setup>
+import {ref, watch} from 'vue'
+import PlusSvg from './svgs/PlusSvg.vue'
+const memoContent = ref("")
+
+const placeholderText = 'メモを入力してください...\n' +
+    '　(Enterで保存、Shift+Enterで改行)'
+
+const isButtonDisabled = ref(true)
+function judgeDisabled(memoContent){
+    isButtonDisabled.value = memoContent.length <= 0;
+}
+watch(memoContent,judgeDisabled)
+
+</script>
+
+<template>
+    <div class="max-w-xl mx-auto border bg-white rounded-xl shadow-sm py-6 px-6 my-8">
+        <div class="flex gap-2 items-center mb-4">
+            <PlusSvg />
+            <p class="text-lg font-bold">新しいメモ</p>
+        </div>
+        <div>
+            <form action="">
+                <textarea v-model="memoContent" :placeholder="placeholderText" class="w-full border rounded-xl resize-none focus:border-blue-400 outline-none mb-4 h-32 p-4"></textarea><br/>
+                <button :disabled="isButtonDisabled" class="border rounded-xl w-full bg-orange-500 text-white disabled:opacity-50 disabled:cursor-not-allowed py-3">＋ メモを保存</button>
+            </form>
+        </div>
+    </div>
+</template>
--- laravel/resources/js/pages/index.vue
+++ laravel/resources/js/pages/index.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 //import Welcome from "../features/Welcome.vue";
 import Header from '../components/Header.vue'
+import TextareaForm from '../components/TextareaForm.vue'
 </script>
 
 <template>
   <Header />
+    <TextareaForm />
 </template>
