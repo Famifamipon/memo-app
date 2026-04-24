ab808233c691a2a7a1ffccb33acbcac502ed06d0
Famifamipon <uraraka0124@gmail.com>
2026-04-24 17:27:25 +0900

    メモ表示のUIを作成

--- laravel/resources/js/components/MemoList.vue
+++ laravel/resources/js/components/MemoList.vue
@@ -1,6 +1,7 @@
 <script setup>
 import {ref} from 'vue'
 import DocumentSvg from './svgs/DocumentSvg.vue'
+import TrashSvg from './svgs/TrashSvg.vue'
 
 const memos = ref([
     {
@@ -16,11 +17,19 @@ const memos = ref([
 ])
 </script>
 
-<template class="">
-    <div>
-        <div class="flex gap-2 items-center">
+<template>
+    <div class="max-w-xl mx-auto">
+        <div class="flex gap-2 items-center mb-4">
             <DocumentSvg class="w-6 h-6"/>
             <p class="text-lg font-bold">保存されたメモ</p>
+        </div>
+        <div v-for="memo in memos" :key="memo.id" class="border rounded-xl shadow-sm mb-4 relative group">
+            <p class="m-4">{{memo.content}}</p>
+            <p class="text-xs text-slate-400 mb-4 mx-4">{{memo.created_time}}</p>
+
+            <button class="absolute top-4 right-4 invisible group-hover:visible text-slate-400 p-2">
+                <TrashSvg />
+            </button>
         </div>
     </div>
 </template>
