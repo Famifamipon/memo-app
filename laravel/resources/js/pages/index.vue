5cc6e2c7838d4cf104249ef9e8692d984a455d89
Famifamipon <uraraka0124@gmail.com>
2026-05-07 14:44:24 +0900

    App.vueにHeaderとFooterを移動した

--- laravel/resources/js/pages/index.vue
+++ laravel/resources/js/pages/index.vue
@@ -1,16 +1,12 @@
 <script setup lang="ts">
-import Header from '../components/Header.vue'
 import TextareaForm from '../components/TextareaForm.vue'
 import MemoList from '../components/MemoList.vue'
-import Footer from '../components/Footer.vue'
 
 </script>
 
 <template>
-  <Header />
     <main class="bg-orange-50 py-4 min-h-screen">
         <TextareaForm />
         <MemoList />
     </main>
-    <Footer />
 </template>
